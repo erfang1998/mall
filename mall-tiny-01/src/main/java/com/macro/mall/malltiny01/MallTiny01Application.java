8b9508da776892d6a24fee8fc670ea9ca000fbeb
erfang <erfang1998@gmail.com>
2019-12-31 16:16:45 +0800

    test1

--- mall-tiny-01/src/main/java/com/macro/mall/malltiny01/MallTiny01Application.java
+++ mall-tiny-01/src/main/java/com/macro/mall/malltiny01/MallTiny01Application.java
@@ -9,8 +9,9 @@ public class MallTiny01Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
         SpringApplication.run(MallTiny01Application.class, args);
-        System.out.println("args = " + args);
-
+        System.out.println("args = " + args.toString());
+        System.out.println("just test git push");
+        System.out.println("just test git push 2.0");
     }
     //第一行
 }
